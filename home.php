80b2da95b462b5e9311bfef57bd795c808a0f431
xManuelG <96047242+xManuelG@users.noreply.github.com>
2021-12-13 03:00:10 -0600

    Home
    contendra parte de la arquitectura del html

---
+++ home.php
@@ -0,0 +1,15 @@
+
+
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HOME</title>
+</head>
+<body>
+<h1>BIENVENIDO</h1>
+    
+</body>
+</html>
